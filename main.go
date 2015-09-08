88dccf55d16529a8ff069282c5f176c7559b56c4
Tero Marttila <terom@fixme.fi>
2015-09-08 23:40:53 +0300

    main: --http-listen and manually dump host interface, bridge_fdb tables

--- main.go
+++ main.go
@@ -1,26 +1,43 @@
 package main
 
 import (
-    "github.com/qmsk/snmpbot/snmp"
     "github.com/jessevdk/go-flags"
+    "net/http"
+    "io"
     "encoding/json"
     "log"
     "os"
+    "github.com/qmsk/snmpbot/snmp"
+)
+
+const (
+    HTTP_LISTEN = ":8085"
 )
 
 type options struct {
+    HttpListen      string `long:"http-listen" description:"HTTP listen address"`
+
     Args    struct {
         HostsJson   flags.Filename `short:"H" long:"hosts-json" description:"Path to hosts .json"`
     } `positional-args:"yes" required:"yes"`
 }
 
 /* Set of active SNMP hosts */
-type Hosts struct {
-    Hosts map[string]*snmp.Client
+type Host struct {
+    snmpClient  *snmp.Client
+
+    Interfaces  snmp.InterfaceTable
+    BridgeFdb   snmp.Bridge_FdbTable
 }
 
-func (self *Hosts) loadJsonFile (file *os.File) error {
-    decoder := json.NewDecoder(file)
+type State struct {
+    hosts       map[string]*Host
+
+    httpServer  *http.Server
+}
+
+func (self *State) loadHostsJson (stream io.Reader) error {
+    decoder := json.NewDecoder(stream)
     configs := make(map[string]snmp.Config)
 
     if err := decoder.Decode(&configs); err != nil {
@@ -28,7 +45,7 @@ func (self *Hosts) loadJsonFile (file *os.File) error {
     }
 
     // host Clients
-    self.Hosts = make(map[string]*snmp.Client)
+    self.hosts = make(map[string]*Host)
 
     for name, config := range configs {
         if client, err := config.Connect(); err != nil {
@@ -36,53 +53,84 @@ func (self *Hosts) loadJsonFile (file *os.File) error {
         } else {
             log.Printf("Client %s: Connect %s\n", name, config)
 
-            self.Hosts[name] = client
+            self.hosts[name] = &Host{
+                snmpClient: client,
+
+                Interfaces: make(snmp.InterfaceTable),
+                BridgeFdb: make(snmp.Bridge_FdbTable),
+            }
         }
     }
 
     return nil
 }
 
+// http entry point
+type hostEntry struct{
+    Interfaces  []*snmp.InterfaceEntry
+    BridgeFdb   []*snmp.Bridge_FdbEntry
+}
+
+func (self *State) handleHttp (response http.ResponseWriter, request *http.Request) {
+    response.Header().Set("Content-Type", "text/json")
+
+    hosts := make(map[string]hostEntry)
+
+    for hostName, host := range self.hosts {
+        var hostEntry hostEntry
+
+        // update
+        host.snmpClient.GetTable(&host.Interfaces)
+        host.snmpClient.GetTable(&host.BridgeFdb)
+
+        for _, entry := range host.Interfaces {
+            hostEntry.Interfaces = append(hostEntry.Interfaces, entry)
+        }
+        for _, entry := range host.BridgeFdb {
+            hostEntry.BridgeFdb = append(hostEntry.BridgeFdb, entry)
+        }
+
+        hosts[hostName] = hostEntry
+    }
+
+    if err := json.NewEncoder(response).Encode(hosts); err != nil {
+        log.Printf("State.handleHttp: json.Encode: %s\n", err)
+    }
+}
+
 func main () {
-    var options options
-    var hosts Hosts
+    var options options = options{
+        HttpListen: HTTP_LISTEN,
+    }
+    var state State
 
     if args, err := flags.Parse(&options); err != nil {
-        //log.Printf("Options: %s\n", err)
+        log.Printf("Options: %s\n", err)
         os.Exit(1)
     } else {
         log.Printf("Options: %+v %+v\n", options, args)
     }
 
+    // hosts from json
     if file, err := os.Open((string)(options.Args.HostsJson)); err != nil {
         log.Printf("Open --hosts-json: %s\n", err)
         os.Exit(1)
-    } else if err := hosts.loadJsonFile(file); err != nil {
+    } else if err := state.loadHostsJson(file); err != nil {
         log.Printf("Load --hosts-json=%s: %s\n", options.Args.HostsJson, err)
         os.Exit(2)
     } else {
-        log.Printf("Hosts: %+v\n", hosts)
+        log.Printf("Hosts: %+v\n", state.hosts)
     }
 
-    for hostName, hostClient := range hosts.Hosts {
-        if interfaces, err := hostClient.Interfaces(); err != nil {
-            log.Printf("Host %s: SNMP Interfaces: %s\n", hostName, err)
-        } else {
-            log.Printf("Host %s: SNMP Interfaces:\n", hostName)
+    // http server
+    state.httpServer = &http.Server{
+        Addr:   options.HttpListen,
+    }
 
-            for index, iface := range interfaces {
-                log.Printf("\t%3d: %+v\n", index, iface)
-            }
-        }
+    http.HandleFunc("/", state.handleHttp)
 
-        if table, err := hostClient.Bridge_FDB(); err != nil {
-            log.Printf("Host %s: SNMP Bridge FDB: %s\n", hostName, err)
-        } else {
-            log.Printf("Host %s: SNMP Bridge FDB:\n", hostName)
-
-            for index, entry := range table {
-                log.Printf("\t%v: %+v\n", index, entry)
-            }
-        }
+    if err := state.httpServer.ListenAndServe(); err != nil {
+        log.Printf("Start --http-listen=%s: %s\n", options.HttpListen, err)
+        os.Exit(1)
     }
 }
