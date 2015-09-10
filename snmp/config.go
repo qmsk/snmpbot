43e740be8f19d46c375a04ccc3cbbe1799fc36a6
Tero Marttila <terom@fixme.fi>
2015-09-11 00:17:13 +0300

    snmp config: Host and Port

--- snmp/config.go
+++ snmp/config.go
@@ -3,16 +3,18 @@ package snmp
 import (
     "fmt"
     "log"
+    "net"
     "net/url"
 )
 
 const (
     COMMUNITY   = "public"
+    PORT        = "161"
 )
 
 // Parse a pseudo-URL config string:
 //  [community "@"] Host
-func ParseConfig(str string, base Config) (config Config, err error) {
+func ParseConfig(str string, baseConfig Config) (config Config, err error) {
     str = "snmp://" + str
 
     configUrl, err := url.Parse(str)
@@ -22,24 +24,34 @@ func ParseConfig(str string, base Config) (config Config, err error) {
 
     if configUrl.User != nil {
         config.Community = configUrl.User.Username()
-    } else if base.Community != "" {
-        config.Community = base.Community
+    } else if baseConfig.Community != "" {
+        config.Community = baseConfig.Community
     } else {
         config.Community = COMMUNITY
     }
 
     log.Printf("ParseConfig %s: url=%#v\n", str, configUrl)
 
-    config.Host = configUrl.Host
+    if host, port, err := net.SplitHostPort(configUrl.Host); err == nil {
+        config.Host = host
+        config.Port = port
+    } else if baseConfig.Port != "" {
+        config.Host = configUrl.Host
+        config.Port = baseConfig.Port
+    } else {
+        config.Host = configUrl.Host
+        config.Port = PORT
+    }
 
     return config, nil
 }
 
 type Config struct {
-    Host        string  `json:host`
     Community   string  `json:community`
+    Host        string  `json:host`
+    Port        string  `json:port`
 }
 
 func (self Config) String() string {
-    return fmt.Sprintf("%s@%s", self.Community, self.Host)
+    return fmt.Sprintf("%s@%s:%s", self.Community, self.Host, self.Port)
 }
