d6dc42bd0beee3d7477e21cd82a42f05917dddc3
Tero Marttila <terom@fixme.fi>
2015-09-13 20:17:42 +0000

    snmp snmpv2: snmpTrapOID is not an object

--- snmp/snmpv2.go
+++ snmp/snmpv2.go
@@ -4,6 +4,8 @@ var (
     SNMPv2MIB               = registerMIB("SNMPv2-MIB", OID{1,3,6,1,6,3,1})
     SNMPv2_system           = registerMIB("system",     OID{1,3,6,1,2,1,1})
 
+    SNMPv2_snmpTrapOID      = SNMPv2MIB.define(1,4,1)
+
     SNMPv2_sysDescr         = SNMPv2_system.registerObject("sysDescr",          StringSyntax,       SNMPv2_system.define(1))
     SNMPv2_sysObjectID      = SNMPv2_system.registerObject("sysObjectID",       OIDSyntax,          SNMPv2_system.define(2))
     SNMPv2_sysUpTime        = SNMPv2_system.registerObject("sysUpTime",         TimeTicksSyntax,    SNMPv2_system.define(3))
@@ -13,7 +15,6 @@ var (
     SNMPv2_sysServices      = SNMPv2_system.registerObject("sysServices",       IntegerSyntax,      SNMPv2_system.define(7))
     SNMPv2_sysORLastChange  = SNMPv2_system.registerObject("sysORLastChange",   TimeTicksSyntax,    SNMPv2_system.define(8))
 
-    SNMPv2_snmpTrapOID              = SNMPv2MIB.registerObject("trapOID",       OIDSyntax,          SNMPv2MIB.define(1,4,1))        // 1.3.6.1.6.3.1.1.4.1
     SNMPv2_coldStart                = SNMPv2MIB.registerNotificationType("coldStart",               SNMPv2MIB.define(1,5,1))    // 1.3.6.1.6.3.1.1.5.1
     SNMPv2_warmStart                = SNMPv2MIB.registerNotificationType("warmStart",               SNMPv2MIB.define(1,5,2))
     SNMPv2_authenticationFailure    = SNMPv2MIB.registerNotificationType("authenticationFailure",   SNMPv2MIB.define(1,5,5))
