87743dc5eeee3fb67be9f471f49012b879055e5e
Tero Marttila <terom@fixme.fi>
2017-10-28 15:05:47 +0300

    split up snmp packages

--- snmp/client.go
+++ snmp/client.go
@@ -293,69 +293,11 @@ func (self *Client) requestGet(requestType wapsnmp.BERType, oids []OID) ([]VarBi
 func (self *Client) Get(oids ...OID) ([]VarBind, error) {
 	self.log.Printf("Get %v\n", oids)
 
-	return self.requestGet(wapsnmp.AsnGetRequest, oids)
+	return self.requestGet(GetRequestType, oids)
 }
 
 func (self *Client) GetNext(oids ...OID) ([]VarBind, error) {
 	self.log.Printf("GetNext %v\n", oids)
 
 	return self.requestGet(wapsnmp.AsnGetNextRequest, oids)
-}
-
-// Get a scalar SNMP object, returning its value
-// Returns nil if the object is not found
-func (self *Client) GetObject(object *Object) (interface{}, error) {
-	if varBinds, err := self.Get(object.OID.define(0)); err != nil {
-		return nil, err
-	} else {
-		for _, varBind := range varBinds {
-			oid := OID(varBind.Name)
-
-			if index := object.Index(oid); index == nil {
-				return nil, fmt.Errorf("response var-bind OID mismatch")
-			} else if varBind.Value == wapsnmp.NoSuchObject || varBind.Value == wapsnmp.NoSuchInstance {
-				return nil, nil
-			} else if objectValue, err := object.ParseValue(varBind.Value); err != nil {
-				return nil, err
-			} else {
-				return objectValue, nil
-			}
-		}
-
-		return nil, nil
-	}
-}
-
-// Probe for supported MIBS
-func (self *Client) ProbeMIBs(handler func(*MIB)) error {
-	for _, mib := range mibs {
-		// TODO: probe...
-		handler(mib)
-	}
-
-	return nil
-}
-
-// Probe for supported Objects in given MIB
-func (self *Client) ProbeMIBObjects(mib *MIB, handler func(*Object)) error {
-	for _, object := range mib.objects {
-		if object.Table != nil {
-			continue
-		}
-
-		// TODO: probe...
-		handler(object)
-	}
-
-	return nil
-}
-
-// Probe for supported Tables in given MIB
-func (self *Client) ProbeMIBTables(mib *MIB, handler func(*Table)) error {
-	for _, table := range mib.tables {
-		// TODO: probe...
-		handler(table)
-	}
-
-	return nil
 }
