8c794625f9e3b9076a1f36a5b58aaec9098e3fff
Tero Marttila <terom@fixme.fi>
2017-10-28 15:38:53 +0300

    test unmarshal with response packet

--- snmp_new/packet_test.go
+++ snmp_new/packet_test.go
@@ -31,6 +31,13 @@ a1 14												-- GetNextRequest-PDU
       05 00												-- NULL value
 `)
 
+var testGetResponsePacket = decodeTestPacket(`
+30 38 02 01 01 04 06 70 75 62 6c 69 63 a2 2b 02
+04 01 7a 6d f3 02 01 00 02 01 00 30 1d 30 1b 06
+08 2b 06 01 02 01 01 05 00 04 0f 55 42 4e 54 20
+45 64 67 65 53 77 69 74 63 68
+`)
+
 func TestPacketMarshal(t *testing.T) {
 	var packet = Packet{Version: SNMPv2c, Community: []byte("public")}
 	var pdu = PDU{
@@ -63,13 +70,16 @@ func TestPacketUnmarshal(t *testing.T) {
 		Community: []byte("public"),
 	}
 	var expectedPDU = PDU{
-		RequestID: 1337,
+		RequestID: 24800755,
 		VarBinds: []VarBind{
-			VarBind{Name: OID{1, 3, 6}},
+			VarBind{
+				Name:  OID{1, 3, 6, 1, 2, 1, 1, 5, 0},
+				Value: []byte("UBNT EdgeSwitch"),
+			},
 		},
 	}
 
-	if err := packet.unmarshal(testGetNextRequestPacket); err != nil {
+	if err := packet.unmarshal(testGetResponsePacket); err != nil {
 		t.Errorf("packet.unmarshal: %v", err)
 		return
 	}
@@ -81,6 +91,6 @@ func TestPacketUnmarshal(t *testing.T) {
 
 	assert.Equal(t, expectedPacket.Version, packet.Version)
 	assert.Equal(t, expectedPacket.Community, packet.Community)
-	assert.Equal(t, GetNextRequestType, packet.PDUType())
+	assert.Equal(t, GetResponseType, packet.PDUType())
 	assert.Equal(t, expectedPDU, pdu)
 }
