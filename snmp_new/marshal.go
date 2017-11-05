246c208395ff6aa8295be305c0f425a03407b94b
Tero Marttila <terom@fixme.fi>
2017-11-05 14:08:21 +0200

    snmp: VarBind set/marshal

--- snmp_new/marshal.go
+++ snmp_new/marshal.go
@@ -4,6 +4,27 @@ import (
 	"encoding/asn1"
 )
 
+// XXX: workaround lack of asn1.MarshalWithParameters
+func pack(cls int, tag int, value interface{}) (asn1.RawValue, error) {
+	var raw asn1.RawValue
+
+	if value == nil {
+		raw = asn1.NullRawValue
+	} else if buf, err := asn1.Marshal(value); err != nil {
+		return raw, err
+	} else if _, err := asn1.Unmarshal(buf, &raw); err != nil {
+		return raw, err
+	}
+
+	if cls != asn1.ClassUniversal {
+		raw.Class = cls
+		raw.Tag = tag
+		raw.FullBytes = nil
+	}
+
+	return raw, nil
+}
+
 func packSequence(cls int, tag int, values ...interface{}) (asn1.RawValue, error) {
 	var raw = asn1.RawValue{Class: cls, Tag: tag, IsCompound: true}
 
@@ -45,4 +66,55 @@ func (pdu PDU) Pack(pduType PDUType) (asn1.RawValue, error) {
 		pdu.ErrorIndex,
 		pdu.VarBinds,
 	)
+}
+
+func (varBind *VarBind) Set(genericValue interface{}) error {
+	switch value := genericValue.(type) {
+	case nil:
+		varBind.SetNull()
+	case ErrorValue:
+		return varBind.SetError(value)
+	case IPAddress:
+		return varBind.setApplication(IPAddressType, value)
+	case Counter32:
+		return varBind.setApplication(Counter32Type, int(value))
+	case Gauge32:
+		return varBind.setApplication(Gauge32Type, int(value))
+	case TimeTicks32:
+		return varBind.setApplication(TimeTicks32Type, int(value))
+	case Opaque:
+		return varBind.setApplication(OpaqueType, value)
+	default:
+		if rawValue, err := pack(asn1.ClassUniversal, 0, value); err != nil {
+			return err
+		} else {
+			varBind.RawValue = rawValue
+		}
+	}
+
+	return nil
+}
+
+func (varBind *VarBind) SetNull() {
+	varBind.RawValue = asn1.NullRawValue
+}
+
+func (varBind *VarBind) SetError(errorValue ErrorValue) error {
+	if rawValue, err := pack(asn1.ClassContextSpecific, int(errorValue), nil); err != nil {
+		return err
+	} else {
+		varBind.RawValue = rawValue
+	}
+
+	return nil
+}
+
+func (varBind *VarBind) setApplication(tag ApplicationValueType, value interface{}) error {
+	if rawValue, err := pack(asn1.ClassApplication, int(tag), value); err != nil {
+		return err
+	} else {
+		varBind.RawValue = rawValue
+	}
+
+	return nil
 }
--- snmp_new/packet_test.go
+++ snmp_new/packet_test.go
@@ -85,6 +85,35 @@ func testPacketUnmarshal(t *testing.T, test packetTest) {
 	}
 }
 
+func TestPacketMarshal(t *testing.T) {
+	testPacketMarshal(t, packetTest{
+		bytes: decodeTestPacket(`
+			30 21 											-- SEQUENCE
+			02 01 01 										-- INTEGER version
+			04 06 70 75 62 6c 69 63 		-- OCTET STRING community
+			a1 14												-- GetNextRequest-PDU
+			  02 02 05 39									-- INTEGER request-id
+			  02 01 00										-- INTEGER error-status
+			  02 01 00										-- INTEGER error-index
+			  30 08												-- SEQUENCE variable-bindings
+			    30 06												-- SEQUENCE
+			      06 02 2b 06									-- OID name
+			      05 00												-- NULL value
+		`),
+		packet: Packet{
+			Version:   SNMPv2c,
+			Community: []byte("public"),
+		},
+		pduType: GetNextRequestType,
+		pdu: PDU{
+			RequestID: 1337,
+			VarBinds: []VarBind{
+				VarBind{Name: OID{1, 3, 6}},
+			},
+		},
+	})
+}
+
 func TestPacketUnmarshal(t *testing.T) {
 	testPacketUnmarshal(t, packetTest{
 		bytes: decodeTestPacket(`
