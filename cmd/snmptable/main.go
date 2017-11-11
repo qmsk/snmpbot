f1537aa82c50c796bcfa256bbe739efaceedc17f
Tero Marttila <terom@fixme.fi>
2017-11-11 16:54:12 +0200

    cmd/snmptable: tab alignment

--- cmd/snmptable/main.go
+++ cmd/snmptable/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"github.com/qmsk/snmpbot/cmd"
 	"github.com/qmsk/snmpbot/mibs"
+	"os"
+	"text/tabwriter"
 )
 
 type Options struct {
@@ -17,31 +19,51 @@ func init() {
 }
 
 func snmptable(client *mibs.Client, id mibs.ID) error {
-	table := id.Table()
+	var table = id.Table()
+	var writer = tabwriter.NewWriter(os.Stdout, 8, 4, 1, ' ', 0)
 
 	if table == nil {
 		return fmt.Errorf("Not a table: %v", id)
 	}
 
-  for _, indexObject := range table.IndexSyntax {
-    fmt.Printf("%v\t", indexObject)
-  }
-  for _, entryObject := range table.EntrySyntax {
-    fmt.Printf("\t%v", entryObject)
-  }
-  fmt.Printf("\n")
+	for _, indexObject := range table.IndexSyntax {
+		fmt.Fprintf(writer, "%v\t", indexObject)
+	}
+	fmt.Fprintf(writer, "|")
+	for _, entryObject := range table.EntrySyntax {
+		fmt.Fprintf(writer, "\t%v", entryObject)
+	}
+	fmt.Fprintf(writer, "\n")
 
-	return client.WalkTable(table, func(indexMap mibs.IndexMap, entryMap mibs.EntryMap) error {
-    for _, indexObject := range table.IndexSyntax {
-      fmt.Printf("%v\t", indexMap[indexObject.ID.Key()])
-    }
-    for _, entryObject := range table.EntrySyntax {
-      fmt.Printf("%v\t", entryMap[entryObject.ID.Key()])
-    }
-    fmt.Printf("\n")
+	for range table.IndexSyntax {
+		fmt.Fprintf(writer, "---\t")
+	}
+	fmt.Fprintf(writer, "|")
+	for range table.EntrySyntax {
+		fmt.Fprintf(writer, "\t---")
+	}
+	fmt.Fprintf(writer, "\n")
+
+	walkRow := func(indexMap mibs.IndexMap, entryMap mibs.EntryMap) error {
+		for _, indexObject := range table.IndexSyntax {
+			fmt.Fprintf(writer, "%v\t", indexMap[indexObject.ID.Key()])
+		}
+		fmt.Fprintf(writer, "|")
+		for _, entryObject := range table.EntrySyntax {
+			fmt.Fprintf(writer, "\t%v", entryMap[entryObject.ID.Key()])
+		}
+		fmt.Fprintf(writer, "\n")
 
 		return nil
-	})
+	}
+
+	if err := client.WalkTable(table, walkRow); err != nil {
+		return err
+	}
+
+	writer.Flush()
+
+	return nil
 }
 
 func main() {
