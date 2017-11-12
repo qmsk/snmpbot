0f9bccf680d410957b2b3d57a0da8e3c50b952f8
Tero Marttila <terom@fixme.fi>
2017-11-12 02:42:29 +0200

    client: partial walks

--- client/client_test.go
+++ client/client_test.go
@@ -1,6 +1,7 @@
 package client
 
 import (
+	"fmt"
 	"github.com/qmsk/snmpbot/snmp"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
@@ -139,6 +140,33 @@ func (transport *testTransport) mockGetNext(oid snmp.OID, varBind snmp.VarBind) 
 	})
 }
 
+func (transport *testTransport) mockGetNextMulti(oids []snmp.OID, varBinds []snmp.VarBind) {
+	var requestVars = make([]snmp.VarBind, len(oids))
+	for i, oid := range oids {
+		requestVars[i] = snmp.MakeVarBind(oid, nil)
+	}
+
+	transport.On("GetNextRequest", IO{
+		Packet: snmp.Packet{
+			Version:   snmp.SNMPv2c,
+			Community: []byte("public"),
+		},
+		PDUType: snmp.GetNextRequestType,
+		PDU: snmp.PDU{
+			VarBinds: requestVars,
+		},
+	}).Return(error(nil), IO{
+		Packet: snmp.Packet{
+			Version:   snmp.SNMPv2c,
+			Community: []byte("public"),
+		},
+		PDUType: snmp.GetResponseType,
+		PDU: snmp.PDU{
+			VarBinds: varBinds,
+		},
+	})
+}
+
 func assertVarBind(t *testing.T, varBinds []snmp.VarBind, index int, expectedOID snmp.OID, expectedValue interface{}) {
 	if len(varBinds) < index {
 		t.Errorf("VarBinds[%d]: short %d", index, len(varBinds))
@@ -220,6 +248,38 @@ func TestWalkV2(t *testing.T) {
 			return nil
 		}, oid); err != nil {
 			t.Fatalf("Walk(%v): %v", oid, err)
+		}
+	})
+}
+
+func TestWalkPartial(t *testing.T) {
+	var oid1 = snmp.OID{1, 3, 6, 1, 2, 1, 17, 7, 1, 2, 2, 1, 1} // Q-BRIDGE-MIB::dot1qTpFdbAddress (not-accessible)
+	var oid2 = snmp.OID{1, 3, 6, 1, 2, 1, 17, 7, 1, 2, 2, 1, 2} // Q-BRIDGE-MIB::dot1qTpFdbPort
+
+	var varBinds = []snmp.VarBind{
+		snmp.MakeVarBind(snmp.OID{1, 3, 6, 1, 2, 1, 17, 7, 1, 2, 2, 1, 2, 1, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11}, int(1)),
+		snmp.MakeVarBind(snmp.OID{1, 3, 6, 1, 2, 1, 17, 7, 1, 2, 2, 1, 2, 1, 0x22, 0x22, 0x22, 0x22, 0x22, 0x22}, int(3)),
+		snmp.MakeVarBind(snmp.OID{1, 3, 6, 1, 2, 1, 17, 7, 1, 2, 2, 1, 3, 1, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11}, int(1)),
+	}
+
+	withTestClient(t, func(transport *testTransport, client *Client) {
+		var walkMock mock.Mock
+		defer walkMock.AssertExpectations(t)
+
+		transport.mockGetNextMulti([]snmp.OID{oid1, oid2}, []snmp.VarBind{varBinds[0], varBinds[0]})
+		walkMock.On("walk[1/1]", varBinds[0])
+		transport.mockGetNextMulti([]snmp.OID{oid1, varBinds[0].OID()}, []snmp.VarBind{varBinds[0], varBinds[1]})
+		walkMock.On("walk[1/1]", varBinds[1])
+		transport.mockGetNextMulti([]snmp.OID{oid1, varBinds[1].OID()}, []snmp.VarBind{varBinds[0], varBinds[2]})
+
+		if err := client.Walk(func(varBinds ...snmp.VarBind) error {
+			for i, varBind := range varBinds {
+				walkMock.MethodCalled(fmt.Sprintf("walk[%d/%d]", i+1, len(varBinds)), varBind)
+			}
+
+			return nil
+		}, oid1, oid2); err != nil {
+			t.Fatalf("Walk(%v, %v): %v", oid1, oid2, err)
 		}
 	})
 }
--- client/walk.go
+++ client/walk.go
@@ -4,6 +4,8 @@ import (
 	"github.com/qmsk/snmpbot/snmp"
 )
 
+// Only walks over VarBinds that are within the requested OID prefix.
+// May walk with fewer VarBinds if some of them do not have any sub-objects.
 func (client *Client) Walk(walkFunc func(...snmp.VarBind) error, startOIDs ...snmp.OID) error {
 	var oids = make([]snmp.OID, len(startOIDs))
 
@@ -15,23 +17,31 @@ func (client *Client) Walk(walkFunc func(...snmp.VarBind) error, startOIDs ...sn
 		if varBinds, err := client.GetNext(oids...); err != nil {
 			return err
 		} else {
+			// omit vars that walked out of the table
+			var valid = make([]snmp.VarBind, 0, len(varBinds))
+
 			for i, varBind := range varBinds {
 				oid := varBind.OID()
 
 				if errorValue := varBind.ErrorValue(); errorValue == snmp.EndOfMibViewValue {
 					// explicit SNMPv2 break
-					return nil
+					continue
 				} else if oid.Equals(oids[i]) || startOIDs[i].Index(oid) == nil {
 					// not making progress, or walked out of tree
-					return nil
+					continue
 				} else {
 					oids[i] = oid
+					valid = append(valid, varBind)
 				}
 			}
 
-			if err := walkFunc(varBinds...); err != nil {
+			if len(valid) == 0 {
+				break
+			} else if err := walkFunc(valid...); err != nil {
 				return err
 			}
 		}
 	}
+
+	return nil
 }
