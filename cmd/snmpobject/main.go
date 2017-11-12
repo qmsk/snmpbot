7fcdaace66f463120a1f1c4c2e7266e84244b8fe
Tero Marttila <terom@fixme.fi>
2017-11-12 13:16:31 +0200

    snmpobject: GetObject() at index .0

---
+++ cmd/snmpobject/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"github.com/qmsk/snmpbot/cmd"
+	"github.com/qmsk/snmpbot/mibs"
+)
+
+type Options struct {
+	cmd.Options
+}
+
+var options Options
+
+func init() {
+	options.InitFlags()
+}
+
+func snmpobject(client *mibs.Client, id mibs.ID) error {
+	var object = id.Object()
+
+	if object == nil {
+		return fmt.Errorf("Not an object: %v", id)
+	}
+
+	if value, err := client.GetObject(object); err != nil {
+		return err
+	} else {
+    fmt.Printf("%v = %v\n", object, value)
+  }
+
+	return nil
+}
+
+func main() {
+	options.Main(func(args []string) error {
+		return options.WithClientID(args, snmpobject)
+	})
+}
--- mibs/client.go
+++ mibs/client.go
@@ -9,6 +9,17 @@ type Client struct {
 	*client.Client
 }
 
+// Read the value at object index .0
+func (client *Client) GetObject(object *Object) (Value, error) {
+	if varBinds, err := client.Get(object.OID.Extend(0)); err != nil {
+		return nil, err
+	} else if value, err := object.Unpack(varBinds[0]); err != nil {
+		return nil, err
+	} else {
+		return value, nil
+	}
+}
+
 func (client *Client) WalkTable(table *Table, f func(IndexMap, EntryMap) error) error {
 	return client.Walk(func(varBinds ...snmp.VarBind) error {
 		if indexMap, entryMap, err := table.Map(varBinds); err != nil {
