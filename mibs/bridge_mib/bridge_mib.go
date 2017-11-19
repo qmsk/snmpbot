9e780bf12e5e6ca1a38152cdcb4ec1a404cb491e
Tero Marttila <terom@fixme.fi>
2017-11-19 14:42:34 +0200

    mibs/bridge_mib: fix object IndexSyntax

--- mibs/bridge_mib/bridge_mib.go
+++ mibs/bridge_mib/bridge_mib.go
@@ -73,9 +73,11 @@ var (
 		EntrySyntax: mibs.EntrySyntax{
 			dot1dStpPort,
 			MIB.RegisterObject(dot1dStpPortEntry.MakeID("dot1dStpPortPriority", 2), mibs.Object{
-				Syntax: mibs.IntegerSyntax{},
+				IndexSyntax: dot1dStpPortIndexSyntax,
+				Syntax:      mibs.IntegerSyntax{},
 			}),
 			MIB.RegisterObject(dot1dStpPortEntry.MakeID("dot1dStpPortState", 3), mibs.Object{
+				IndexSyntax: dot1dStpPortIndexSyntax,
 				Syntax: mibs.EnumSyntax{
 					{1, "disabled"},
 					{2, "blocking"},
@@ -86,28 +88,35 @@ var (
 				},
 			}),
 			MIB.RegisterObject(dot1dStpPortEntry.MakeID("dot1dStpPortEnable", 4), mibs.Object{
+				IndexSyntax: dot1dStpPortIndexSyntax,
 				Syntax: mibs.EnumSyntax{
 					{1, "enabled"},
 					{2, "disabled"},
 				},
 			}),
 			MIB.RegisterObject(dot1dStpPortEntry.MakeID("dot1dStpPortPathCost", 5), mibs.Object{
-				Syntax: mibs.IntegerSyntax{},
+				IndexSyntax: dot1dStpPortIndexSyntax,
+				Syntax:      mibs.IntegerSyntax{},
 			}),
 			MIB.RegisterObject(dot1dStpPortEntry.MakeID("dot1dStpPortDesignatedRoot", 6), mibs.Object{
-				Syntax: BridgeIDSyntax{},
+				IndexSyntax: dot1dStpPortIndexSyntax,
+				Syntax:      BridgeIDSyntax{},
 			}),
 			MIB.RegisterObject(dot1dStpPortEntry.MakeID("dot1dStpPortDesignatedCost", 7), mibs.Object{
-				Syntax: mibs.IntegerSyntax{},
+				IndexSyntax: dot1dStpPortIndexSyntax,
+				Syntax:      mibs.IntegerSyntax{},
 			}),
 			MIB.RegisterObject(dot1dStpPortEntry.MakeID("dot1dStpPortDesignatedBridge", 8), mibs.Object{
-				Syntax: BridgeIDSyntax{},
+				IndexSyntax: dot1dStpPortIndexSyntax,
+				Syntax:      BridgeIDSyntax{},
 			}),
 			MIB.RegisterObject(dot1dStpPortEntry.MakeID("dot1dStpPortDesignatedPort", 9), mibs.Object{
-				Syntax: mibs.OctetStringSyntax{},
+				IndexSyntax: dot1dStpPortIndexSyntax,
+				Syntax:      mibs.OctetStringSyntax{},
 			}),
 			MIB.RegisterObject(dot1dStpPortEntry.MakeID("dot1dStpPortForwardTransitions", 10), mibs.Object{
-				Syntax: mibs.CounterSyntax{},
+				IndexSyntax: dot1dStpPortIndexSyntax,
+				Syntax:      mibs.CounterSyntax{},
 			}),
 			// dot1dStpPortPathCost32 XXX: compat issues
 		},
@@ -126,17 +135,20 @@ var (
 	dot1dTpFdbAddress = MIB.RegisterObject(dot1dTpFdbEntry.MakeID("dot1dTpFdbAddress", 1), mibs.Object{
 		Syntax: mibs.MACAddressSyntax{},
 	})
+	dot1dTpFdbIndexSyntax = mibs.IndexSyntax{
+		dot1dTpFdbAddress,
+	}
 
 	dot1dTpFdbTable = MIB.RegisterTable(dot1dTp.MakeID("dot1dTpFdbTable", 3), mibs.Table{
-		IndexSyntax: mibs.IndexSyntax{
-			dot1dTpFdbAddress,
-		},
+		IndexSyntax: dot1dTpFdbIndexSyntax,
 		EntrySyntax: mibs.EntrySyntax{
 			dot1dTpFdbAddress,
 			MIB.RegisterObject(dot1dTpFdbEntry.MakeID("dot1dTpFdbPort", 2), mibs.Object{
-				Syntax: mibs.IntegerSyntax{},
+				IndexSyntax: dot1dTpFdbIndexSyntax,
+				Syntax:      mibs.IntegerSyntax{},
 			}),
 			MIB.RegisterObject(dot1dTpFdbEntry.MakeID("dot1dTpFdbStatus", 3), mibs.Object{
+				IndexSyntax: dot1dTpFdbIndexSyntax,
 				Syntax: mibs.EnumSyntax{
 					{1, "other"},
 					{2, "invalid"},
@@ -148,3 +160,8 @@ var (
 		},
 	})
 )
+
+func init() {
+	dot1dStpPort.IndexSyntax = dot1dStpPortIndexSyntax
+	dot1dTpFdbAddress.IndexSyntax = dot1dTpFdbIndexSyntax
+}
