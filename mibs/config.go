5085377a7c1e6f25893ec9d1e56ae901d21bf627
Tero Marttila <terom@fixme.fi>
2018-02-03 13:01:25 +0200

    mibs: load mibs from JSON config files

---
+++ mibs/config.go
@@ -0,0 +1,168 @@
+package mibs
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/qmsk/snmpbot/snmp"
+	"io"
+	"os"
+	"path/filepath"
+)
+
+type ConfigID struct {
+	OID  string
+	Name string
+}
+
+func (config ConfigID) resolve(mib *MIB) (ID, error) {
+	if id, err := mib.Resolve(config.OID); err != nil {
+		return id, err
+	} else {
+		return ID{MIB: mib, OID: id.OID, Name: config.Name}, nil
+	}
+}
+
+type MIBConfig struct {
+	OID     string
+	Name    string
+	Objects []ObjectConfig
+	Tables  []TableConfig
+
+	oid snmp.OID
+}
+
+func (config MIBConfig) build() (MIB, error) {
+	var id = ID{Name: config.Name}
+
+	if oid, err := snmp.ParseOID(config.OID); err != nil {
+		return MIB{}, fmt.Errorf("Invalid OID for MIB %v: %v", config.Name, err)
+	} else {
+		id.OID = oid
+	}
+
+	return makeMIB(id), nil
+}
+
+type ObjectConfig struct {
+	ConfigID
+	Syntax        string
+	SyntaxOptions json.RawMessage // TODO
+	NotAccessible bool
+}
+
+func (config ObjectConfig) build(mib *MIB) (Object, error) {
+	var object = Object{
+		NotAccessible: config.NotAccessible,
+	}
+
+	if id, err := config.resolve(mib); err != nil {
+		return object, err
+	} else {
+		object.ID = id
+	}
+
+	if syntax, err := LookupSyntax(config.Syntax); err != nil {
+		return object, err
+	} else {
+		object.Syntax = syntax
+	}
+
+	return object, nil
+}
+
+type TableConfig struct {
+	ConfigID
+	IndexObjects []string
+	EntryObjects []string
+}
+
+func (config TableConfig) build(mib *MIB) (Table, error) {
+	var table = Table{
+		IndexSyntax: make(IndexSyntax, len(config.IndexObjects)),
+		EntrySyntax: make(EntrySyntax, len(config.EntryObjects)),
+	}
+
+	if id, err := config.resolve(mib); err != nil {
+		return table, err
+	} else {
+		table.ID = id
+	}
+
+	for i, indexName := range config.IndexObjects {
+		if indexObject := mib.ResolveObject(indexName); indexObject == nil {
+			return table, fmt.Errorf("Unknown IndexObject: %v", indexName)
+		} else {
+			table.IndexSyntax[i] = indexObject
+		}
+	}
+
+	for i, entryName := range config.EntryObjects {
+		if entryObject := mib.ResolveObject(entryName); entryObject == nil {
+			return table, fmt.Errorf("Unknown EntryObject: %v", entryName)
+		} else {
+			table.EntrySyntax[i] = entryObject
+		}
+	}
+
+	return table, nil
+}
+
+func loadMIB(config MIBConfig) (*MIB, error) {
+	var mib *MIB
+
+	if buildMIB, err := config.build(); err != nil {
+		return nil, err
+	} else {
+		mib = registerMIB(buildMIB)
+	}
+
+	for _, objectConfig := range config.Objects {
+		if object, err := objectConfig.build(mib); err != nil {
+			return mib, fmt.Errorf("Invalid Object %v: %v", objectConfig.Name, err)
+		} else {
+			mib.registerObject(object)
+		}
+	}
+
+	for _, tableConfig := range config.Tables {
+		if table, err := tableConfig.build(mib); err != nil {
+			return mib, fmt.Errorf("Invalid Table %v: %v", tableConfig.Name, err)
+		} else {
+			mib.registerTable(table)
+
+			// setup object IndexSyntax
+			for _, indexObject := range table.IndexSyntax {
+				indexObject.IndexSyntax = table.IndexSyntax
+			}
+
+			for _, entryObject := range table.EntrySyntax {
+				entryObject.IndexSyntax = table.IndexSyntax
+			}
+		}
+	}
+
+	return mib, nil
+}
+
+func LoadJSON(r io.Reader) (*MIB, error) {
+	var config MIBConfig
+
+	if err := json.NewDecoder(r).Decode(&config); err != nil {
+		return nil, err
+	}
+
+	return loadMIB(config)
+}
+
+func Load(path string) (*MIB, error) {
+	if file, err := os.Open(path); err != nil {
+		return nil, err
+	} else {
+		switch ext := filepath.Ext(path); ext {
+		case ".json":
+			return LoadJSON(file)
+		default:
+			return nil, fmt.Errorf("Unknown MIB file extension: %v", ext)
+		}
+	}
+}
---
+++ mibs/config_test.go
@@ -0,0 +1,68 @@
+package mibs
+
+import (
+	"github.com/qmsk/snmpbot/snmp"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestConfigLoadMIB(t *testing.T) {
+	if mib, err := Load("test/TEST2-MIB.json"); err != nil {
+		t.Fatalf("Load test/TEST2-MIB.json: %v", err)
+	} else {
+		assert.Equal(t, "TEST2-MIB", mib.String())
+
+		if resolveMib, err := ResolveMIB("TEST2-MIB"); err != nil {
+			t.Errorf("ResolveMIB TEST2-MIB: %v", err)
+		} else {
+			assert.Equal(t, mib, resolveMib)
+		}
+
+		if id, err := Resolve("TEST2-MIB"); err != nil {
+			t.Errorf("Resolve TEST2-MIB: %v", err)
+		} else {
+			assert.Equal(t, "TEST2-MIB", id.String())
+		}
+
+		if id, err := Resolve("TEST2-MIB::test"); err != nil {
+			t.Errorf("Resolve TEST2-MIB::test: %v", err)
+		} else {
+			assert.Equal(t, "TEST2-MIB::test", id.String())
+		}
+
+		if id, err := Resolve("TEST2-MIB::testTable"); err != nil {
+			t.Errorf("Resolve TEST2-MIB::testTable: %v", err)
+		} else {
+			assert.Equal(t, "TEST2-MIB::testTable", id.String())
+		}
+
+		if object, err := ResolveObject("TEST2-MIB::test"); err != nil {
+			t.Errorf("ResolveObject TEST2-MIB::test: %v", err)
+		} else {
+			assert.Equal(t, "TEST2-MIB::test", object.String())
+			assert.Equal(t, DisplayStringSyntax{}, object.Syntax)
+		}
+
+		if table, err := ResolveTable("TEST2-MIB::testTable"); err != nil {
+			t.Errorf("ResolveTable TEST2-MIB::testTable: %v", err)
+		} else {
+			assert.Equal(t, "TEST2-MIB::testTable", table.String())
+			assert.Equal(t, IndexSyntax{mib.ResolveObject("testID")}, table.IndexSyntax)
+			assert.Equal(t, EntrySyntax{mib.ResolveObject("testName")}, table.EntrySyntax)
+		}
+
+		if object, err := ResolveObject("TEST2-MIB::testName"); err != nil {
+			t.Errorf("ResolveObject TEST2-MIB::testName: %v", err)
+		} else {
+			assert.Equal(t, "TEST2-MIB::testName", object.String())
+			assert.Equal(t, DisplayStringSyntax{}, object.Syntax)
+			assert.Equal(t, IndexSyntax{mib.ResolveObject("testID")}, object.IndexSyntax)
+		}
+
+		if object := LookupObject(snmp.OID{1, 0, 2, 1, 1}); object == nil {
+			t.Errorf("LookupObject .1.0.2.1.1: %v", nil)
+		} else {
+			assert.Equal(t, "TEST2-MIB::test", object.String())
+		}
+	}
+}
