7f9cd75cb394e0b245d3d4fe901219c005b2e229
Tero Marttila <terom@fixme.fi>
2018-02-18 23:30:49 +0200

    mibs: split up config tests

--- mibs/config_test.go
+++ mibs/config_test.go
@@ -7,130 +7,154 @@ import (
 	"testing"
 )
 
-func TestConfigLoadMIB(t *testing.T) {
+func init() {
 	if err := Load("test/TEST2-MIB.json"); err != nil {
-		t.Fatalf("Load test/TEST2-MIB.json: %v", err)
+		panic(err)
+	}
+}
+
+func TestConfigResolveMIB(t *testing.T) {
+	if resolveMIB, err := ResolveMIB("TEST2-MIB"); err != nil {
+		t.Errorf("ResolveMIB TEST2-MIB: %v", err)
 	} else {
-		var mib *MIB
+		assert.Equal(t, "TEST2-MIB", resolveMIB.String())
+	}
+}
 
-		if resolveMIB, err := ResolveMIB("TEST2-MIB"); err != nil {
-			t.Errorf("ResolveMIB TEST2-MIB: %v", err)
+func TestConfigResolve(t *testing.T) {
+	if id, err := Resolve("TEST2-MIB"); err != nil {
+		t.Errorf("Resolve TEST2-MIB: %v", err)
+	} else {
+		assert.Equal(t, "TEST2-MIB", id.String())
+	}
+}
+
+func TestConfigResolveObjectID(t *testing.T) {
+	if id, err := Resolve("TEST2-MIB::test"); err != nil {
+		t.Errorf("Resolve TEST2-MIB::test: %v", err)
+	} else {
+		assert.Equal(t, "TEST2-MIB::test", id.String())
+	}
+}
+
+func TestConfigResolveTableID(t *testing.T) {
+	if id, err := Resolve("TEST2-MIB::testTable"); err != nil {
+		t.Errorf("Resolve TEST2-MIB::testTable: %v", err)
+	} else {
+		assert.Equal(t, "TEST2-MIB::testTable", id.String())
+	}
+}
+
+func TestConfigResolveObject(t *testing.T) {
+	if object, err := ResolveObject("TEST2-MIB::test"); err != nil {
+		t.Errorf("ResolveObject TEST2-MIB::test: %v", err)
+	} else {
+		assert.Equal(t, "TEST2-MIB::test", object.String())
+		assert.Equal(t, &DisplayStringSyntax{}, object.Syntax)
+	}
+}
+
+func TestConfigResolveTable(t *testing.T) {
+	mib := LookupMIB(snmp.OID{1, 0, 2})
+
+	if table, err := ResolveTable("TEST2-MIB::testTable"); err != nil {
+		t.Errorf("ResolveTable TEST2-MIB::testTable: %v", err)
+	} else {
+		assert.Equal(t, "TEST2-MIB::testTable", table.String())
+		assert.Equal(t, IndexSyntax{mib.ResolveObject("testID")}, table.IndexSyntax)
+		assert.Equal(t, EntrySyntax{mib.ResolveObject("testName")}, table.EntrySyntax)
+	}
+}
+
+func TestConfigObjectIndexSyntax(t *testing.T) {
+	mib := LookupMIB(snmp.OID{1, 0, 2})
+
+	if object, err := ResolveObject("TEST2-MIB::testName"); err != nil {
+		t.Errorf("ResolveObject TEST2-MIB::testName: %v", err)
+	} else {
+		assert.Equal(t, "TEST2-MIB::testName", object.String())
+		assert.Equal(t, &DisplayStringSyntax{}, object.Syntax)
+		assert.Equal(t, IndexSyntax{mib.ResolveObject("testID")}, object.IndexSyntax)
+
+		var varBind = snmp.MakeVarBind(object.OID.Extend(10), []byte("foobar"))
+
+		if name, value, err := object.Format(varBind); err != nil {
+			t.Errorf("Object<%v>.Format %v: %v", object, varBind, err)
 		} else {
-			assert.Equal(t, "TEST2-MIB", resolveMIB.String())
-
-			mib = resolveMIB
+			assert.Equal(t, "TEST2-MIB::testName[10]", name)
+			assert.Equal(t, "foobar", fmt.Sprintf("%v", value))
 		}
+	}
+}
 
-		if id, err := Resolve("TEST2-MIB"); err != nil {
-			t.Errorf("Resolve TEST2-MIB: %v", err)
+func TestConfigObjectEnumSyntax(t *testing.T) {
+	if object, err := ResolveObject("TEST2-MIB::testEnum"); err != nil {
+		t.Errorf("ResolveObject TEST2-MIB::testEnum: %v", err)
+	} else {
+		assert.Equal(t, "TEST2-MIB::testEnum", object.String())
+		assert.Equal(t, &EnumSyntax{
+			{1, "one"},
+			{2, "two"},
+		}, object.Syntax)
+
+		var varBind = snmp.MakeVarBind(object.OID.Extend(0), int(1))
+
+		if name, value, err := object.Format(varBind); err != nil {
+			t.Errorf("Object<%v>.Format %v: %v", object, varBind, err)
 		} else {
-			assert.Equal(t, "TEST2-MIB", id.String())
+			assert.Equal(t, "TEST2-MIB::testEnum", name)
+			assert.Equal(t, "one", fmt.Sprintf("%v", value))
 		}
+	}
+}
 
-		if id, err := Resolve("TEST2-MIB::test"); err != nil {
-			t.Errorf("Resolve TEST2-MIB::test: %v", err)
+func TestConfigResolveIDExternal(t *testing.T) {
+	if object, err := ResolveObject("TEST2-MIB::extObject"); err != nil {
+		t.Errorf("ResolveObject TEST2-MIB::extObject: %v", err)
+	} else {
+		assert.Equal(t, "TEST2-MIB::extObject", object.String())
+	}
+}
+
+func TestConfigLookupObject(t *testing.T) {
+	if object := LookupObject(snmp.OID{1, 0, 2, 1, 1}); object == nil {
+		t.Errorf("LookupObject .1.0.2.1.1: %v", nil)
+	} else {
+		assert.Equal(t, "TEST2-MIB::test", object.String())
+
+		var varBind = snmp.MakeVarBind(object.OID.Extend(0), []byte("foobar"))
+
+		if name, value, err := object.Format(varBind); err != nil {
+			t.Errorf("Object<%v>.Format %v: %v", object, varBind, err)
 		} else {
-			assert.Equal(t, "TEST2-MIB::test", id.String())
+			assert.Equal(t, "TEST2-MIB::test", name)
+			assert.Equal(t, "foobar", fmt.Sprintf("%v", value))
 		}
+	}
+}
 
-		if id, err := Resolve("TEST2-MIB::testTable"); err != nil {
-			t.Errorf("Resolve TEST2-MIB::testTable: %v", err)
+func TestConfigLookupObjectExt(t *testing.T) {
+	if object := LookupObject(snmp.OID{1, 1, 5, 1}); object == nil {
+		t.Errorf("LookupObject .1.1.5.1: %v", nil)
+	} else {
+		assert.Equal(t, "TEST2-MIB::extObject", object.String())
+	}
+}
+
+func TestConfigObjectUnknownSyntax(t *testing.T) {
+	if object, err := ResolveObject("TEST2-MIB::testUnknownSyntax"); err != nil {
+		t.Errorf("ResolveObject TEST2-MIB::testUnknownSyntax: %v", err)
+	} else {
+		assert.Equal(t, "TEST2-MIB::testUnknownSyntax", object.String())
+		assert.Equal(t, nil, object.Syntax)
+
+		var varBind = snmp.MakeVarBind(object.OID.Extend(0), int(1))
+
+		if name, value, err := object.Format(varBind); err != nil {
+			t.Errorf("Object<%v>.Format %v: %v", object, varBind, err)
 		} else {
-			assert.Equal(t, "TEST2-MIB::testTable", id.String())
-		}
-
-		if object, err := ResolveObject("TEST2-MIB::test"); err != nil {
-			t.Errorf("ResolveObject TEST2-MIB::test: %v", err)
-		} else {
-			assert.Equal(t, "TEST2-MIB::test", object.String())
-			assert.Equal(t, &DisplayStringSyntax{}, object.Syntax)
-		}
-
-		if table, err := ResolveTable("TEST2-MIB::testTable"); err != nil {
-			t.Errorf("ResolveTable TEST2-MIB::testTable: %v", err)
-		} else {
-			assert.Equal(t, "TEST2-MIB::testTable", table.String())
-			assert.Equal(t, IndexSyntax{mib.ResolveObject("testID")}, table.IndexSyntax)
-			assert.Equal(t, EntrySyntax{mib.ResolveObject("testName")}, table.EntrySyntax)
-		}
-
-		if object, err := ResolveObject("TEST2-MIB::testName"); err != nil {
-			t.Errorf("ResolveObject TEST2-MIB::testName: %v", err)
-		} else {
-			assert.Equal(t, "TEST2-MIB::testName", object.String())
-			assert.Equal(t, &DisplayStringSyntax{}, object.Syntax)
-			assert.Equal(t, IndexSyntax{mib.ResolveObject("testID")}, object.IndexSyntax)
-
-			var varBind = snmp.MakeVarBind(object.OID.Extend(10), []byte("foobar"))
-
-			if name, value, err := object.Format(varBind); err != nil {
-				t.Errorf("Object<%v>.Format %v: %v", object, varBind, err)
-			} else {
-				assert.Equal(t, "TEST2-MIB::testName[10]", name)
-				assert.Equal(t, "foobar", fmt.Sprintf("%v", value))
-			}
-		}
-
-		if object, err := ResolveObject("TEST2-MIB::testEnum"); err != nil {
-			t.Errorf("ResolveObject TEST2-MIB::testEnum: %v", err)
-		} else {
-			assert.Equal(t, "TEST2-MIB::testEnum", object.String())
-			assert.Equal(t, &EnumSyntax{
-				{1, "one"},
-				{2, "two"},
-			}, object.Syntax)
-
-			var varBind = snmp.MakeVarBind(object.OID.Extend(0), int(1))
-
-			if name, value, err := object.Format(varBind); err != nil {
-				t.Errorf("Object<%v>.Format %v: %v", object, varBind, err)
-			} else {
-				assert.Equal(t, "TEST2-MIB::testEnum", name)
-				assert.Equal(t, "one", fmt.Sprintf("%v", value))
-			}
-		}
-
-		if object, err := ResolveObject("TEST2-MIB::extObject"); err != nil {
-			t.Errorf("ResolveObject TEST2-MIB::extObject: %v", err)
-		} else {
-			assert.Equal(t, "TEST2-MIB::extObject", object.String())
-		}
-
-		if object := LookupObject(snmp.OID{1, 0, 2, 1, 1}); object == nil {
-			t.Errorf("LookupObject .1.0.2.1.1: %v", nil)
-		} else {
-			assert.Equal(t, "TEST2-MIB::test", object.String())
-
-			var varBind = snmp.MakeVarBind(object.OID.Extend(0), []byte("foobar"))
-
-			if name, value, err := object.Format(varBind); err != nil {
-				t.Errorf("Object<%v>.Format %v: %v", object, varBind, err)
-			} else {
-				assert.Equal(t, "TEST2-MIB::test", name)
-				assert.Equal(t, "foobar", fmt.Sprintf("%v", value))
-			}
-		}
-
-		if object := LookupObject(snmp.OID{1, 1, 5, 1}); object == nil {
-			t.Errorf("LookupObject .1.1.5.1: %v", nil)
-		} else {
-			assert.Equal(t, "TEST2-MIB::extObject", object.String())
-		}
-
-		if object, err := ResolveObject("TEST2-MIB::testUnknownSyntax"); err != nil {
-			t.Errorf("ResolveObject TEST2-MIB::testUnknownSyntax: %v", err)
-		} else {
-			assert.Equal(t, "TEST2-MIB::testUnknownSyntax", object.String())
-			assert.Equal(t, nil, object.Syntax)
-
-			var varBind = snmp.MakeVarBind(object.OID.Extend(0), int(1))
-
-			if name, value, err := object.Format(varBind); err != nil {
-				t.Errorf("Object<%v>.Format %v: %v", object, varBind, err)
-			} else {
-				assert.Equal(t, "TEST2-MIB::testUnknownSyntax", name)
-				assert.Equal(t, "1", fmt.Sprintf("%v", value))
-			}
+			assert.Equal(t, "TEST2-MIB::testUnknownSyntax", name)
+			assert.Equal(t, "1", fmt.Sprintf("%v", value))
 		}
 	}
 }
