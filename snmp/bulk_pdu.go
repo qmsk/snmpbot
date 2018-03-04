ed5823f469a891c67d70e0e5237849c74b51dfd3
Tero Marttila <terom@fixme.fi>
2018-03-05 00:49:34 +0200

    snmp: BulkPDU.String()

--- snmp/bulk_pdu.go
+++ snmp/bulk_pdu.go
@@ -2,6 +2,8 @@ package snmp
 
 import (
 	"encoding/asn1"
+	"fmt"
+	"strings"
 )
 
 // Very similar to the PDU type, but the error fields are replaced by parameters
@@ -21,6 +23,21 @@ func (pdu BulkPDU) GetRequestID() int {
 }
 func (pdu BulkPDU) SetRequestID(id int) {
 	pdu.RequestID = id
+}
+
+func (pdu BulkPDU) String() string {
+	var scalars []string
+	var entries []string
+
+	for i, varBind := range pdu.VarBinds {
+		if i < pdu.NonRepeaters {
+			scalars = append(scalars, varBind.String())
+		} else {
+			entries = append(entries, varBind.String())
+		}
+	}
+
+	return fmt.Sprintf("[%v] + %dx[%v]", strings.Join(scalars, ", "), pdu.MaxRepetitions, strings.Join(entries, ", "))
 }
 
 func (pdu BulkPDU) GetError() PDUError {
