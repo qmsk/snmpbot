d157ac9b76a1e1b001fb24126fd33508a7474748
Tero Marttila <terom@fixme.fi>
2020-08-22 20:52:59 +0300

    server: add more hosts API coverage

--- server/hosts_test.go
+++ server/hosts_test.go
@@ -166,3 +166,200 @@ func TestEnginePostHost(t *testing.T) {
 	assert.Equal(t, testHostIndex, apiHostIndex, "response host")
 	assert.ElementsMatch(t, []HostID{HostID("test")}, engine.Hosts().Keys(), "engine.Hosts")
 }
+
+func TestEnginePostHostCommunity(t *testing.T) {
+	var engine = makeTestEngine(testConfig{
+		hosts: map[HostID]HostConfig{},
+	})
+
+	//
+	var apiHostIndex api.HostIndex
+	var testHostIndex = api.HostIndex{
+		ID:       "test",
+		SNMP:     "private@localhost",
+		Online:   true,
+		Location: "test",
+	}
+
+	webtest.TestAPI(t, webtest.APITest{
+		Handler: WebAPI(engine),
+		Request: webtest.APIRequest{
+			Method: "POST",
+			Target: "/hosts/",
+			Object: api.HostPOST{
+				ID:        "test",
+				SNMP:      "localhost",
+				Community: "private",
+				Location:  "test",
+			},
+		},
+		Response: webtest.APIResponse{
+			StatusCode: 200,
+			Object:     &apiHostIndex,
+		},
+	})
+
+	assert.Equal(t, testHostIndex, apiHostIndex, "response host")
+	assert.ElementsMatch(t, []HostID{HostID("test")}, engine.Hosts().Keys(), "engine.Hosts")
+}
+
+func TestEnginePostHostConflict(t *testing.T) {
+	var engine = makeTestEngine(testConfig{
+		hosts: map[HostID]HostConfig{
+			HostID("test"): HostConfig{
+				SNMP:     "public@localhost",
+				Location: "test",
+			},
+		},
+	})
+
+	//
+	webtest.TestAPI(t, webtest.APITest{
+		Handler: WebAPI(engine),
+		Request: webtest.APIRequest{
+			Method: "POST",
+			Target: "/hosts/",
+			Object: api.HostPOST{
+				ID:       "test",
+				SNMP:     "public@localhost",
+				Location: "test",
+			},
+		},
+		Response: webtest.APIResponse{
+			StatusCode: 409,
+		},
+	})
+
+	assert.ElementsMatch(t, []HostID{HostID("test")}, engine.Hosts().Keys(), "engine.Hosts")
+}
+
+func TestEnginePutHost(t *testing.T) {
+	var engine = makeTestEngine(testConfig{
+		hosts: map[HostID]HostConfig{
+			HostID("test1"): HostConfig{
+				SNMP:     "public@localhost",
+				Location: "test",
+			},
+		},
+	})
+
+	//
+	var apiHostIndex api.HostIndex
+	var testHostIndex = api.HostIndex{
+		ID:       "test2",
+		SNMP:     "private@localhost",
+		Online:   true,
+		Location: "test",
+	}
+	webtest.TestAPI(t, webtest.APITest{
+		Handler: WebAPI(engine),
+		Request: webtest.APIRequest{
+			Method: "PUT",
+			Target: "/hosts/test2",
+			Object: api.HostPOST{
+				ID:       "test2",
+				SNMP:     "private@localhost",
+				Location: "test",
+			},
+		},
+		Response: webtest.APIResponse{
+			StatusCode: 200,
+			Object:     &apiHostIndex,
+		},
+	})
+
+	assert.Equal(t, testHostIndex, apiHostIndex, "response host")
+	assert.ElementsMatch(t, []HostID{HostID("test1"), HostID("test2")}, engine.Hosts().Keys(), "engine.Hosts")
+	assert.Equal(t, "public@localhost", engine.Hosts()["test1"].client.String(), "engine.Host test1 SNMP")
+	assert.Equal(t, "private@localhost", engine.Hosts()["test2"].client.String(), "engine.Host test2 SNMP")
+}
+
+func TestEnginePutHostUpdate(t *testing.T) {
+	var engine = makeTestEngine(testConfig{
+		hosts: map[HostID]HostConfig{
+			HostID("test"): HostConfig{
+				SNMP:     "public@localhost",
+				Location: "test",
+			},
+		},
+	})
+
+	//
+	var apiHostIndex api.HostIndex
+	var testHostIndex = api.HostIndex{
+		ID:       "test",
+		SNMP:     "private@localhost",
+		Online:   true,
+		Location: "test",
+	}
+	webtest.TestAPI(t, webtest.APITest{
+		Handler: WebAPI(engine),
+		Request: webtest.APIRequest{
+			Method: "PUT",
+			Target: "/hosts/test",
+			Object: api.HostPOST{
+				ID:       "test",
+				SNMP:     "private@localhost",
+				Location: "test",
+			},
+		},
+		Response: webtest.APIResponse{
+			StatusCode: 200,
+			Object:     &apiHostIndex,
+		},
+	})
+
+	assert.Equal(t, testHostIndex, apiHostIndex, "response host")
+	assert.ElementsMatch(t, []HostID{HostID("test")}, engine.Hosts().Keys(), "engine.Hosts")
+	assert.Equal(t, "private@localhost", engine.Hosts()["test"].client.String(), "engine.Host test SNMP")
+}
+
+func TestEngineDeleteHost(t *testing.T) {
+	var engine = makeTestEngine(testConfig{
+		hosts: map[HostID]HostConfig{
+			HostID("test"): HostConfig{
+				SNMP:     "public@localhost",
+				Location: "test",
+			},
+		},
+	})
+
+	//
+	webtest.TestAPI(t, webtest.APITest{
+		Handler: WebAPI(engine),
+		Request: webtest.APIRequest{
+			Method: "DELETE",
+			Target: "/hosts/test",
+		},
+		Response: webtest.APIResponse{
+			StatusCode: 204,
+		},
+	})
+
+	assert.ElementsMatch(t, []HostID{}, engine.Hosts().Keys(), "engine.Hosts")
+}
+
+func TestEngineDeleteUnkonwnHost(t *testing.T) {
+	var engine = makeTestEngine(testConfig{
+		hosts: map[HostID]HostConfig{
+			HostID("test"): HostConfig{
+				SNMP:     "public@localhost",
+				Location: "test",
+			},
+		},
+	})
+
+	//
+	webtest.TestAPI(t, webtest.APITest{
+		Handler: WebAPI(engine),
+		Request: webtest.APIRequest{
+			Method: "DELETE",
+			Target: "/hosts/test2",
+		},
+		Response: webtest.APIResponse{
+			StatusCode: 404,
+		},
+	})
+
+	assert.ElementsMatch(t, []HostID{HostID("test")}, engine.Hosts().Keys(), "engine.Hosts")
+}
